892a3246dabd3889a596f251b9c5fd7d8e4c0e17
alatysavage254 <briansumukwo23@gmail.com>
2024-12-07 11:36:32 +0300

    first commit

---
+++ CRM/static/js/app.js
@@ -0,0 +1,9 @@
+
+var messages_timeout = document.getElementById("messages_timer");
+
+setTimeout(function()
+
+{
+    messages_timeout.style.display = "none";
+
+}, 5000);
---
+++ main/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Record (models.Model):
+    creation_date =models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=255)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+
+
+    def __str__(self):
+
+        return self.first_name + "   " + self.last_name
+
